908dbad8d30f55561deca30a8c1e448a3e551900
Mads Opheim <mads.opheim@nav.no>
2024-04-25 11:14:45 +0200

    Adding a placeholder file in order to generate javadocs even for the pdfa-testing-module, as it's required by Maven Central, and other ways of bypassing it looks like notably more work

---
+++ openhtmltopdf-pdfa-testing/src/main/java/Placeholder.java
@@ -0,0 +1,6 @@
+public class Placeholder {
+    /*
+     This class is an empty placeholder in order to generate javadocs, which is required by Maven Central for all packages.
+     It's a workaround, but a fairly non-problematic one.
+     */
+}
